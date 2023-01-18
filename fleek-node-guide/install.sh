c089e911177d4bcbb2345f6fdd248b3eec7c68ce
alphahamsterua <alphahamsterua@gmail.com>
2023-01-18 16:49:02 +0200

    Add fleek-node-guide

---
+++ fleek-node-guide/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+echo "Script started..."
+
+echo "Downloading and installing Docker is started..."
+curl -fsSL https://get.docker.com -o get-docker.sh && \
+    sh get-docker.sh && \
+    rm -rf get-docker.sh
+echo "Finished..."
+
+echo "Prepering Docker..."
+usermod -aG docker $USER
+
+systemctl enable docker
+systemctl start docker
+echo "Docker started..."
+
+echo "Downloading ursa repo..."
+wget https://github.com/fleek-network/ursa/archive/refs/heads/main.zip && \
+    unzip main.zip && \
+    rm -rf main.zip && \
+    cd ursa-main
+echo "ursa repo was downloaded..."
+
+echo "Building ursa image..."
+DOCKER_BUILDKIT=1 docker build -t ursa:latest .
+echo "Image was built..."
+
+echo "Starting container..."
+docker run -d -p 4069:4069 -p 6009:6009 -v $HOME/.ursa/:/root/.ursa/:rw --name ursa-cli -it ursa
+echo "Container started..."
+echo "Script finished."
