25427e41a4edc9362ae427f66cb119eddc6bdfc2
alphahamsterua <alphahamsterua@gmail.com>
2023-01-18 16:46:11 +0200

    Add espresso-cape-guide

---
+++ espresso-cape-guide/install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "Script started..."
+echo "Downloading and installing Docker is started..."
+curl -fsSL https://get.docker.com -o get-docker.sh && \
+sh get-docker.sh && \
+rm -rf get-docker.sh
+echo "Finished..."
+
+echo "Prepering Docker..."
+usermod -aG docker $USER
+
+systemctl enable docker
+systemctl start docker
+
+echo "Downloading docker-compose.yaml..."
+curl https://www.espressosys.com/cape/docker-compose.yaml --output docker-compose.yaml
+echo "The file was downloaded..."
+
+docker-compose pull
+docker-compose up -d
+echo "Docker started..."
+
+echo "Script finished."
