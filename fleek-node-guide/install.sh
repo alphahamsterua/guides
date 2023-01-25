f238e19245507cb8f2ed85cb65ef4fd733357d27
alphahamsterua <alphahamsterua@gmail.com>
2023-01-25 16:41:53 +0200

    Minor fix

--- fleek-node-guide/install.sh
+++ fleek-node-guide/install.sh
@@ -4,7 +4,7 @@ echo "Starting install dependencies..."
 apt update
 apt install curl mc wget git htop net-tools unzip jq build-essential ncdu tmux make cmake clang pkg-config libssl-dev protobuf-compiler -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $HOME/.cargo/env
 cargo install sccache
